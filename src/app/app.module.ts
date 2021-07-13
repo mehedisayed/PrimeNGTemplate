386cee94e1ff45dff0822eeaf046432b2d10a967
Md. Raihan Talukder <raihan@aiub.edu>
2021-07-13 11:55:59 +0600

    Working With P-Input-Text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MehediComponent } from './mehedi/mehedi.component';
 import { RaihanComponent } from './raihan/raihan.component';
 import { ButtonModule } from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   declarations: [AppComponent, MehediComponent, RaihanComponent],
-  imports: [BrowserModule, AppRoutingModule, ButtonModule],
+  imports: [BrowserModule,
+        BrowserAnimationsModule, AppRoutingModule, ButtonModule, InputTextModule],
   providers: [],
   bootstrap: [AppComponent],
 })
